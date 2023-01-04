feadaf99c3120244ad3352ff8341a036b4f7b83b
LorenaGomezPerez <lorenagomezperez@gmail.com>
2023-01-04 18:51:07 +0100

    add twitter icon

--- src/Contact.js
+++ src/Contact.js
@@ -45,6 +45,17 @@ function Contact() {
             <i className="fa-brands fa-github icon" />
           </a>
         </div>
+		<div className="iconContainer">
+          <a
+            title="Twitter"
+            href="https://twitter.com/LorenaGP21"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="fa-brands fa-twitter icon"/>
+			
+          </a>
+        </div>
       </div>
     </div>
   );
